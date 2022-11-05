09c645a7d049f2d0f8cbcbae5664430a7b591653
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-11-05 17:27:01 +0100

    Initial commit

---
+++ src/QrSvg.jsx
@@ -0,0 +1,65 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import QRCodeFactory from 'qrcode-generator';
+import { Svg, Path } from 'react-native-svg';
+
+const rect = 'v1h1v-1z';
+
+function makePath(qrcode, margin, reverse) {
+  const moduleCount = qrcode.getModuleCount();
+
+  let d = '';
+
+  for (let row = 0; row < moduleCount; row++) {
+    for (let col = 0; col < moduleCount; col++) {
+      if (qrcode.isDark(row, col) === (reverse ? false : true)) {
+        d += `M${col + margin},${row + margin}${rect}`;
+      }
+    }
+  }
+
+  return d;
+}
+
+export default function QRCode({
+  bgColor = '#fff',
+  cellClassPrefix,
+  fgColor = '#000',
+  level = 'L',
+  margin = 0,
+  type = 0,
+  value = '',
+  ...otherProps
+} = {}) {
+  const qrcode = useMemo(() => {
+    const qrcode = new QRCodeFactory(type, level);
+    qrcode.addData(value);
+    qrcode.make();
+    return qrcode;
+  }, [level, type, value]);
+
+  const size = qrcode.getModuleCount() + margin * 2;
+
+  const bgPath = makePath(qrcode, margin, true);
+  const fgPath = makePath(qrcode, margin);
+
+  const bgClassName = cellClassPrefix && `${cellClassPrefix} ${cellClassPrefix}-empty`;
+  const fgClassName = cellClassPrefix && `${cellClassPrefix} ${cellClassPrefix}-filled`;
+
+  return (
+    <Svg shapeRendering="crispEdges" viewBox={`0 0 ${size} ${size}`} {...otherProps}>
+      <Path d={bgPath} fill={bgColor} className={bgClassName} />
+      <Path d={fgPath} fill={fgColor} className={fgClassName} />
+    </Svg>
+  );
+}
+
+QRCode.propTypes = {
+  bgColor: PropTypes.string,
+  cellClassPrefix: PropTypes.string,
+  fgColor: PropTypes.string,
+  level: PropTypes.oneOf(['L', 'M', 'Q', 'H']),
+  margin: PropTypes.number,
+  type: PropTypes.number,
+  value: PropTypes.string.isRequired,
+};
